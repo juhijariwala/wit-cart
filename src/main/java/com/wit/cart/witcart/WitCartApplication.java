03fd004a0aad24471c059994efe10b7e533ff88b
Neha Arora <neha.arora@thoughtworks.com>
2018-12-04 13:25:31 +0530

    Added dependency for jedis and stored item id in redis

--- src/main/java/com/wit/cart/witcart/WitCartApplication.java
+++ src/main/java/com/wit/cart/witcart/WitCartApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WitCartApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WitCartApplication.class, args);
 	}
--- src/main/java/com/wit/cart/witcart/controller/CartController.java
+++ src/main/java/com/wit/cart/witcart/controller/CartController.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import redis.clients.jedis.Jedis;
 
 @Controller
 @RequestMapping("/")
 public class CartController {
     @GetMapping("cart")
     public ResponseEntity proceedToCart(@RequestParam String item_id){
-        return ResponseEntity.ok().body("items");
+        Jedis client = new Jedis("localhost");
+        client.set("item_id",item_id);
+        return ResponseEntity.ok().body(client.get("item_id"));
     }
 }
