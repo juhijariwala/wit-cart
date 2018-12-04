b658ef840ec33edf2116a32a70e656b259faec31
Neha Arora <neha.arora@thoughtworks.com>
2018-12-04 10:46:53 +0530

    first commit

---
+++ src/main/java/com/wit/cart/witcart/WitCartApplication.java
@@ -0,0 +1,12 @@
+package com.wit.cart.witcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitCartApplication.class, args);
+	}
+}
