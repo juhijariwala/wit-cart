6c276289d49ed34e021374039d4d137a6561069e
Neha Arora <neha.arora@thoughtworks.com>
2018-12-04 19:46:18 +0530

    clearing database to make cart as temporary

--- src/main/java/com/wit/cart/witcart/WitCartApplication.java
+++ src/main/java/com/wit/cart/witcart/WitCartApplication.java
@@ -2,10 +2,13 @@ package com.wit.cart.witcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class WitCartApplication {
 	public static void main(String[] args) {
+		Jedis client = new Jedis("localhost");
+		client.flushDB();
 		SpringApplication.run(WitCartApplication.class, args);
 	}
 }
